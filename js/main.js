ab13a8e4de052c50ecb49e95ee1c67da7b441ec0
proprgrmmr <61632722+sam0132nodier@users.noreply.github.com>
2021-01-08 01:21:07 +0200

    Format the Javascript file

--- js/main.js
+++ js/main.js
@@ -23,43 +23,51 @@ let seconds = 60;
 function countDown() {
 	let minutesStr = '00';
 	seconds--;
-    let secondsStr = (seconds > 9) ? `${seconds}` : `0${seconds}`;
-    document.querySelector('#timer').textContent = `${minutesStr}:${secondsStr}`;
-    
-    if (seconds == 0) {
-        seconds = 60;
-    }
+	let secondsStr = seconds > 9 ? `${seconds}` : `0${seconds}`;
+	document.querySelector('#timer').textContent = `${minutesStr}:${secondsStr}`;
+
+	if (seconds == 56) {
+		document.querySelector('#next').click();
+		seconds = 60;
+	}
+	if (seconds == 0) {
+		seconds = 60;
+	}
 }
 
 // Create an initialize function to format the data
 function initialize(questions) {
 	// Randomly sort questions and select 5 of them.
-	let selected = questions.sort(() => .5 - Math.random()).slice(0, 10);
+	let selected = questions.sort(() => 0.5 - Math.random()).slice(0, 10);
 	// console.log(selected);
 	let selectedHtml = new Array();
 	selected.forEach((question) => {
 		let optionsHtml = [];
-		question.options.forEach ((option) => {
-			optionsHtml.push (`<div>
-				<input type="radio" id="id-${question.options.indexOf(option)}" name="answer" class="checkbox">
-				<label for="id-${question.options.indexOf(option)}"><li>${option}</li></label>
-			</div>`);
+		question.options.forEach((option) => {
+			optionsHtml.push(
+				`<div>
+			        <input type="radio" id="id-${question.options.indexOf(option)}" name="answer" class="checkbox">
+					<label for="id-${question.options.indexOf(option)}">
+						<li>${option}</li>
+					</label>
+				</div>`
+			);
 		});
 		// console.log(optionsHtml);
 		selectedHtml.push(
-		`<p class="strong" id="question-text">${selected.indexOf(question) + 1}. ${question.question}</p>
-			<form action="" method="GET">
-				<ol class="options">
-					${shuffle(optionsHtml).join('\n')}
-				</ol>
-				<hr>
-				<div class="navigation">
-					<div class="timer"><p id="timer">01:00</p></div>
-					<div class="next">
-					<button id="next" type="submit">Next</button>
-					</div>
-				</div>
-			</form>`
+			`<p class="strong" id="question-text">${selected.indexOf(question) + 1}. ${question.question}</p>
+		                  <form action="" method="GET">
+		                  <ol class="options">
+		                  ${shuffle(optionsHtml).join('\n')}
+		                  </ol>
+		                  <hr>
+		                  <div class="navigation">
+		                  <div class="timer"><p id="timer">01:00</p></div>
+		                  <div class="next">
+		                  <button id="next" type="submit">Next</button>
+		                  </div>
+		                  </div>
+		                  </form>`
 		);
 	});
 
@@ -72,28 +80,28 @@ function displayData(html, selected) {
 	if (num < html.length) {
 		document.querySelector('.question').innerHTML = html[num];
 
-        // Create an observer instance linked to the callbackfunction
-        let observer = new MutationObserver(callback);
+		// Create an observer instance linked to the callbackfunction
+		let observer = new MutationObserver(callback);
 
-        // Select the node that'll be used for mutation
-        const timerNode = document.querySelector('#timer');
-        const config = {
-            childList: true,
-            characterData: true,
-            subtree: true
-        };
+		// Select the node that'll be used for mutation
+		const timerNode = document.querySelector('#timer');
+		const config = {
+			childList: true,
+			characterData: true,
+			subtree: true
+		};
 
-        function callback (mutationsList) {
-            for (const mutation of mutationsList) {
-                console.log(mutation.target.textContent);
-                if(mutation.type === 'childList') {
-                    console.log('Node data changed');
-                }
-            }
-        }
+		function callback(mutationsList) {
+			for (const mutation of mutationsList) {
+				console.log(mutation.target.textContent);
+				if (mutation.type === 'childList') {
+					console.log('Node data changed');
+				}
+			}
+		}
 
-        // Start observing the timerNode for configured mutations
-        observer.observe(timerNode, config);
+		// Start observing the timerNode for configured mutations
+		observer.observe(timerNode, config);
 
 		document.addEventListener('click', (e) => {
 			if (e.target.id === 'next') {
@@ -104,7 +112,7 @@ function displayData(html, selected) {
 				e.preventDefault();
 				// Get the form submitted data
 				let optionNum = 1;
-				checkboxes.forEach(each => {
+				checkboxes.forEach((each) => {
 					if (each.checked) {
 						console.log(each.nextElementSibling.textContent);
 						// Grab the selected answer from the checkbox
@@ -115,7 +123,7 @@ function displayData(html, selected) {
 							// console.log(document.querySelector('#question-text').textContent);
 							let questionText = document.querySelector('#question-text').textContent;
 							// console.log(questionText.substring(questionText.indexOf('.') + 2));
-							if (question.question === questionText.substring(questionText.indexOf('.')+2)) {
+							if (question.question === questionText.substring(questionText.indexOf('.') + 2)) {
 								// console.log(question.answer);
 								// console.log(answer);
 								// console.log(question.answer === answer);
@@ -150,48 +158,48 @@ function displayData(html, selected) {
 					// let rating = document.querySelector('#rating');
 
 					let grade = points;
-					let percentage = points * 100 / html.length;
+					let percentage = (points * 100) / html.length;
 
 					let ratingSrc = '';
 					let ratingAlt = '';
 					// Set the stars basing on the percentage
 					if (percentage <= 10) {
-						ratingSrc = "./img/stars1.jpg";
-						ratingAlt = "Really bad";
+						ratingSrc = './img/stars1.jpg';
+						ratingAlt = 'Really bad';
 					} else if (percentage <= 20) {
-						ratingSrc = "./img/stars2.jpg";
-						ratingAlt = "Bad";
+						ratingSrc = './img/stars2.jpg';
+						ratingAlt = 'Bad';
 					} else if (percentage <= 30) {
-						ratingSrc = "./img/stars3.jpg";
-						ratingAlt = "Pretty bad";
+						ratingSrc = './img/stars3.jpg';
+						ratingAlt = 'Pretty bad';
 					} else if (percentage <= 40) {
-						ratingSrc = "./img/stars4.jpg";
-						ratingAlt="Mediocre";
+						ratingSrc = './img/stars4.jpg';
+						ratingAlt = 'Mediocre';
 					} else if (percentage <= 50) {
-						ratingSrc = "./img/stars5.jpg";
-						ratingAlt="Not bad";
+						ratingSrc = './img/stars5.jpg';
+						ratingAlt = 'Not bad';
 					} else if (percentage <= 60) {
-						ratingSrc = "./img/stars6.jpg";
-						ratingAlt="Good";
+						ratingSrc = './img/stars6.jpg';
+						ratingAlt = 'Good';
 					} else if (percentage <= 70) {
-						ratingSrc = "./img/stars7.jpg";
-						ratingAlt="Very good";
+						ratingSrc = './img/stars7.jpg';
+						ratingAlt = 'Very good';
 					} else if (percentage <= 80) {
-						ratingSrc = "./img/stars8.jpg";
-						ratingAlt = "Superb";
+						ratingSrc = './img/stars8.jpg';
+						ratingAlt = 'Superb';
 					} else if (percentage <= 90) {
-						ratingSrc = "./img/stars9.jpg";
-						ratingAlt = "Outstanding";
+						ratingSrc = './img/stars9.jpg';
+						ratingAlt = 'Outstanding';
 					} else {
-						ratingSrc = "./img/stars10.jpg";
-						ratingAlt = "Perfect";
+						ratingSrc = './img/stars10.jpg';
+						ratingAlt = 'Perfect';
 					}
 
 					let gradeHTML = `
-						<img src="${ratingSrc}" alt="${ratingAlt}" id="rating" />
-						<button id="close-grade">+</button>
-						<p class="marks">${grade}/${html.length}</p>
-						<p class="percentage">${percentage}%</p>
+					<img src="${ratingSrc}" alt="${ratingAlt}" id="rating" />
+					<button id="close-grade">+</button>
+					<p class="marks">${grade}/${html.length}</p>
+					<p class="percentage">${percentage}%</p>
 					`;
 					if (!quizWindow.classList.contains('hide')) {
 						quizWindow.classList.add('hide');
@@ -220,13 +228,13 @@ function displayData(html, selected) {
 // Close the window the start button is clicked
 startButton.addEventListener('click', () => {
 	// Fetch data from the json file
-	let url = "../questions.json";
+	let url = '../questions.json';
 	fetch(url).then((response) => {
 		response.json().then((json) => {
 			let questions = json;
 			// console.log(questions);
 			initialize(questions);
-		})
+		});
 	});
 	// num = 0;
 	if (quizWindow.classList.contains('hide')) {
